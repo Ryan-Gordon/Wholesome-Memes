0a13a4fd9c46889f29413cf82b6920350c428260
Ryan Gordon <ryangordon210@gmail.com>
2017-07-05 14:05:28 +0100

    Added a reddit API provider
    Provider added using generate command.
    Citation needed.

---
+++ src/providers/reddit-api/reddit-api.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the RedditApiProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular DI.
+*/
+const BASE_URL: string =  'https://www.reddit.com/';
+const JSON_POSTFIX: string = '.json';
+
+@Injectable()
+export class RedditApiProvider {
+
+  constructor(public http: Http) {}
+
+  fetch(url?: string) {
+    return url ?
+      this.http.get(BASE_URL + '/r/' + url + JSON_POSTFIX)
+        .map(this.redditCollectionToJson) :
+      this.http.get(BASE_URL + JSON_POSTFIX)
+        .map(this.redditCollectionToJson)
+  }
+
+  fetchNext(lastPostName: string, url?: string) {
+    return url ?
+      this.http.get(BASE_URL + '/r/' + url + JSON_POSTFIX + '?count=' + 25 + '&after=' + lastPostName)
+        .map(this.redditCollectionToJson) :
+      this.http.get(BASE_URL + JSON_POSTFIX + '?count=' + 25 + '&after=' + lastPostName)
+        .map(this.redditCollectionToJson)
+  }
+
+  fetchComments(post) {
+    let url: string = BASE_URL + post.permalink + JSON_POSTFIX;
+    return this.http.get(url)
+      .map(res => res.json()[1].data.children.map(c => c.data).filter(c => c.body))
+      .map(this.beautifyReplies.bind(this))
+  }
+
+  beautifyReplies(comments) {
+    return comments.map(comment => {
+      comment.replies = comment.replies ? comment.replies.data.children.map(reply => reply.data).filter(c => c.body) : [];
+      this.beautifyReplies(comment.replies);
+      return comment;
+    })
+  }
+
+  redditCollectionToJson(response) {
+    return response.json().data.children.map(c => c.data)
+  }
+}
